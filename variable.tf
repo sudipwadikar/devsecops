d66f5797a3561e6b87dd41e2729d6c5b7de502d7
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-16 13:13:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "ubuntus1"
+  default     =  "myJune2021Key"
 }
 
 variable "instance_type" {
