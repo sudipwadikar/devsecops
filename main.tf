6d3158c7182926c00f8ee1030702a76df96275da
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-17 10:04:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,11 +40,12 @@ resource "aws_instance" "Instance1" {
   key_name = var.key_name
   instance_type = var.instance_type
   security_groups= [var.security_group]
-  user_data = <<EOF
-  #!/bin/bash
-  sudo apt-get install docker
-  cat /etc/hosts > /home/ubuntu/test.txt
-  EOF
+  user_data = << EOF
+		#! /bin/bash
+    sudo apt-get update
+    sudo apt-get install apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
   tags= {
     Name = var.tag_name
   }
