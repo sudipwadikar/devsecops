f43d58b8270104b2cd36506059e310782dcf4bd3
sudipwadikar <107179469+sudipwadikar@users.noreply.github.com>
2022-06-17 10:07:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,10 +41,10 @@ resource "aws_instance" "Instance1" {
   instance_type = var.instance_type
   security_groups= [var.security_group]
   user_data = << EOF
-		#! /bin/bash
+    #!/bin/bash
     sudo apt-get update
     sudo apt-get install apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+    echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 	EOF
   tags= {
     Name = var.tag_name
